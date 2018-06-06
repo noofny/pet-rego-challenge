2468027c2ab491ad11a6a543d5f7481c77daaa47
Noofny <noofny@gmail.com>
2018-06-06 22:11:03 +1000

    Removed unused config property.

--- src/PetRego/PetRego.Common/Config/AppConfig.cs
+++ src/PetRego/PetRego.Common/Config/AppConfig.cs
@@ -5,7 +5,6 @@ namespace PetRego.Common
     public class AppConfig : IAppConfig
     {
         public string ElasticSearchUri { get; set; }
-        public string BaseUrlPath { get; set; }
         public string TokenizedBaseUrl { get; set; }
 
     }
--- src/PetRego/PetRego.UnitTests/TestConfig.cs
+++ src/PetRego/PetRego.UnitTests/TestConfig.cs
@@ -5,13 +5,11 @@ namespace PetRego.Common
     public class TestConfig : IAppConfig
     {
         public string ElasticSearchUri { get; set; }
-        public string BaseUrlPath { get; set; }
         public string TokenizedBaseUrl { get; set; }
 
         public TestConfig()
         {
             ElasticSearchUri = "http://lcoalhost/doesnt-matter";
-            BaseUrlPath = "api";
             TokenizedBaseUrl = "<<BASE_URL>>";
 
         }
